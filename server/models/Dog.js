cd4f61a49535659bd42acec8b295849dcddb62f0
John Huggins <92586058+PrvCaboose@users.noreply.github.com>
2025-11-02 15:09:07 -0500

    Fixed dog creation/update bugs, added page 4

--- server/models/Dog.js
+++ server/models/Dog.js
@@ -1,30 +1,30 @@
 const mongoose = require('mongoose');
 
-let dogModel = {};
+let DogModel = {};
 
 const DogSchema = new mongoose.Schema({
-    name: {
-        type: String,
-        required: true,
-        trim: true,
-        unique: true
-    },
-    breed: {
-        type: String,
-        required: true,
-        trim: true,
-    },
-    age: {
-        type: Number,
-        min: 0,
-        required: true
-    },
-    createdDate: {
-        type: Date,
-        default: Date.now()
-    }
+  name: {
+    type: String,
+    required: true,
+    trim: true,
+    unique: true,
+  },
+  breed: {
+    type: String,
+    required: true,
+    trim: true,
+  },
+  age: {
+    type: Number,
+    min: 0,
+    required: true,
+  },
+  createdDate: {
+    type: Date,
+    default: Date.now(),
+  },
 });
 
-dogModel = mongoose.model('Dog', DogSchema);
+DogModel = mongoose.model('Dog', DogSchema);
 
-module.exports = dogModel;
+module.exports = DogModel;
